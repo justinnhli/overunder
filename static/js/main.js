9435bea4a49d6fa4961f9248b9bd1a21229005be
Justin Li <justinnhli@gmail.com>
2020-01-17 20:15:11 -0800

    deal with None values in JS

--- static/js/main.js
+++ static/js/main.js
@@ -2,10 +2,16 @@ function save_score(input) {
     input = $(input);
     var alias = input.attr("id").split("__", 1)[0]
     var assignment = input.attr("id").substring(alias.length + 2);
+    var value = input.val();
+    if (input.hasClass("percent_type")) {
+        if (value !== "None" && !value.endsWith("%")) {
+            value = value + "%";
+        }
+    }
     var data = {
         "alias": alias,
         "assignment": assignment,
-        "value": $(input).val()
+        "value": value
     };
     $.post("/save_score", JSON.stringify(data))
         .done(function(response) {
